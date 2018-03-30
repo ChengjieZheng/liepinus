d6fb6fa41222e13a53009bd2d6252e85810ab5b9
Guangyu Xu <davidxu198556@gmail.com>
2018-03-30 17:58:06 -0400

    did server side and client side setup

--- client/app.js
+++ client/app.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import App from './App.jsx'
-import { AppContainer } from 'react-hot-loader'
+import { AppContainer } from 'react-hot-loader' //eslint-disable-line
 
 const root = document.getElementById('root')
 // hot-module-replacement配置
