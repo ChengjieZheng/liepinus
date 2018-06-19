8cc30af19a0384cab05a8e04860c30f4c764b6f7
Chengjie(Jimmy) Zheng <chengjie.zheng001@gmail.com>
2018-06-19 14:12:45 -0400

    change map size

--- client/views/components/map/address-dialog.jsx
+++ client/views/components/map/address-dialog.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -6,6 +8,14 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
 import CompanyAddress from '../map/company-address'
+
+
+const styles = {
+	mapSize: {
+		width: '600px',
+		hight: '550px',
+	},
+}
 
 class AddressDialog extends React.Component {
 	constructor(props) {
@@ -17,17 +27,7 @@ class AddressDialog extends React.Component {
 	}
 
    handleClickOpen() {
-		// const that = this;
-		// function pms1() {
-		// 	return new Promise((resolve, reject) => {
 		this.setState({ open: true });
-		// 		resolve('true');
-		// 	});
-		// }
-		// pms1().then((data) => {
-		// 	console.log(data)
-		// 	document.getElementById('abc').style.visibility = 'visible'
-		// })
 	}
 
   handleClose = () => {
@@ -38,6 +38,7 @@ class AddressDialog extends React.Component {
   };
 
   render() {
+		const { classes } = this.props
     return (
       <div>
         <Button onClick={this.handleClickOpen}>北美猎聘地址: </Button>
@@ -45,21 +46,28 @@ class AddressDialog extends React.Component {
 					id="abc"
           open={this.state.open}
           onClose={this.handleClose}
-          aria-labelledby="responsive-dialog-title"
+					aria-labelledby="responsive-dialog-title"
         >
-          <DialogTitle id="responsive-dialog-title">北美猎聘地址:</DialogTitle>
-          <DialogContent>
-						<CompanyAddress />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color="primary" autoFocus>
-              OK
-            </Button>
-          </DialogActions>
+					<div className={classes.mapSize}>
+						<DialogTitle id="responsive-dialog-title">北美猎聘地址:</DialogTitle>
+						<DialogContent>
+							<CompanyAddress />
+						</DialogContent>
+						<DialogActions>
+							<Button onClick={this.handleClose} color="primary" autoFocus>
+								OK
+							</Button>
+						</DialogActions>
+					</div>
+
         </Dialog>
       </div>
     );
   }
 }
 
-export default AddressDialog
+AddressDialog.protoTypes = {
+	classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(AddressDialog)
